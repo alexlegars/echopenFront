c2e016836154110700a928ed5f95a8b5c1b71c7c
daYungGud <selimxcmr@gmail.com>
2017-07-05 15:36:02 +0200

    slider

--- src/components/BlockFirst.js
+++ src/components/BlockFirst.js
@@ -17,7 +17,7 @@ export default class BlockFirst extends React.Component {
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                     </div>
                     <div className="right">
-                        <img src="" alt=""/>
+                        <img src="http://www.worldwidelearn.com/imagesvr_ce/1428/iStock_DoctorLookingAtChart.jpg" alt=""/>
                     </div>
                     <div className="clear"></div>
                 </div>
@@ -25,7 +25,7 @@ export default class BlockFirst extends React.Component {
 
                 <div className="block__1-second">
                     <div className="left">
-                        <img src="" alt=""/>
+                        <img src="http://www.worldwidelearn.com/imagesvr_ce/1428/iStock_DoctorLookingAtChart.jpg" alt=""/>
                     </div>
                     <div className="right">
                         <h2>Titre en <span>H2</span></h2>
@@ -41,7 +41,7 @@ export default class BlockFirst extends React.Component {
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                     </div>
                     <div className="right">
-                        <img src="" alt=""/>
+                        <img src="http://www.worldwidelearn.com/imagesvr_ce/1428/iStock_DoctorLookingAtChart.jpg" alt=""/>
                     </div>
                     <div className="clear"></div>
                 </div>
@@ -49,7 +49,7 @@ export default class BlockFirst extends React.Component {
 
                 <div className="block__1-second">
                     <div className="left">
-                        <img src="" alt=""/>
+                        <img src="http://www.worldwidelearn.com/imagesvr_ce/1428/iStock_DoctorLookingAtChart.jpg" alt=""/>
                     </div>
                     <div className="right">
                         <h2>Titre en <span>H2</span></h2>
