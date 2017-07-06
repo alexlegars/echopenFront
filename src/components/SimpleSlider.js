4c71c8e49592189317b22172f2de99dc13027575
daYungGud <selimxcmr@gmail.com>
2017-07-06 16:52:22 +0200

    concept part

--- src/components/SimpleSlider.js
+++ src/components/SimpleSlider.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+import $ from 'jquery'
+
 
 export default class SimpleSlider extends React.Component {
     render() {
