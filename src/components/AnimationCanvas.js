1f9a30136161eeb82d65eaf9b648e4fd6ba3ed98
alexlegars <alexlegars@gmail.com>
2017-07-06 13:46:20 +0200

    improve animationCanvas

--- src/components/AnimationCanvas.js
+++ src/components/AnimationCanvas.js
@@ -4,26 +4,10 @@ import SineWaves from 'sine-waves/sine-waves.min'
 import $ from 'jquery';
 
 export default class AnimationCanvas extends React.Component {
-
-    componentDidMount() {
-         this.waves = new SineWaves({
-            el: this.refs.waves,
-
-            speed: 4,
-
-            width: function() {
-                return $(window).width();
-            },
-
-            height: function() {
-                return $(window).height();
-            },
-
-            ease: 'SineInOut',
-
-            wavesWidth: '70%',
-
-            waves: [
+    constructor() {
+        super();
+        this.state = {
+            originalWaves: [
                 {
                     timeModifier: 4,
                     lineWidth: 1,
@@ -51,29 +35,49 @@ export default class AnimationCanvas extends React.Component {
                 {
                     timeModifier: 0.25,
                     lineWidth: 2,
-                    amplitude: -400,
-                    wavelength: 400
+                    amplitude: -170,
+                    wavelength: 170
                 }
-            ],
+            ]
+        }
+
+    }
+    componentDidMount() {
+         this.waves = new SineWaves({
+            el: this.refs.waves,
+
+            speed: 4,
+
+            width: function() {
+                return $(window).width();
+            },
+
+            height: function() {
+                return $(window).height();
+            },
+
+            ease: 'SineInOut',
+
+            wavesWidth: '80%',
+
+            waves: this.props.waves? this.props.waves : this.state.originalWaves,
 
             // Called on window resize
-            resizeEvent: function() {
-                var gradient = this.ctx.createLinearGradient(0, 0, this.width, 0);
-                gradient.addColorStop(0,"rgba(23, 210, 168, 0.2)");
-                gradient.addColorStop(0.5,"rgba(255, 255, 255, 0.5)");
-                gradient.addColorStop(1,"rgba(23, 210, 168, 0.2)");
+             resizeEvent: function() {
+                 var gradient = this.ctx.createLinearGradient(0, 0, this.width, 0);
+                 gradient.addColorStop(1,"rgba(255, 255, 255, 0.2)");
 
-                var index = -1;
-                var length = this.waves.length;
-                while(++index < length){
-                    this.waves[index].strokeStyle = gradient;
-                }
+                 var index = -1;
+                 var length = this.waves.length;
+                 while(++index < length){
+                     this.waves[index].strokeStyle = gradient;
+                 }
 
-                // Clean Up
-                index = void 0;
-                length = void 0;
-                gradient = void 0;
-            }
+                 // Clean Up
+                 index = void 0;
+                 length = void 0;
+                 gradient = void 0;
+             }
         });
     }
     componentWillUnmount() {
