5f6ad570dec1a2d14996dcf269b6b6ac3144bf99
daYungGud <selimxcmr@gmail.com>
2017-07-06 17:04:02 +0200

    concept part

--- src/components/BlockSec.js
+++ src/components/BlockSec.js
@@ -54,7 +54,7 @@ export default class BlockSec extends React.Component {
                                     </h3>
                                     <div className="bottom-part">
                                         <p className="bottom-title">Personnel médical</p>
-                                        <hr/>
+                                        <hr className="hr-1"/>
                                         <p className="bottom-number">01</p>
                                         <div className="clear"></div>
                                     </div>
@@ -74,9 +74,9 @@ export default class BlockSec extends React.Component {
                                     <h3>Roger is fireman and want to improve the diagnostic of his patient during the travel between an accident and the hospital. Now, he participate to our project using an echo-stethoscope and talking around in his work and with his friend !
                                     </h3>
                                     <div className="bottom-part">
-                                        <p className="bottom-title">Personnel médical</p>
-                                        <hr/>
-                                        <p className="bottom-number">01</p>
+                                        <p className="bottom-title">Fireman</p>
+                                        <hr className="hr-2"/>
+                                        <p className="bottom-number">02</p>
                                         <div className="clear"></div>
                                     </div>
                                 </div>
@@ -90,15 +90,15 @@ export default class BlockSec extends React.Component {
 
                         <div className="block__2-wrapper-r">
                             <div className="left">
-                                <h2>Personnel médical</h2>
+                                <h2>Personne lambda</h2>
                                 <div className="left-container">
                                     <h3>Participate to our project is not just using the produce, you can collaborate with us on this project, no matter how !
                                         Your contribution, even the smallest, is important.
                                     </h3>
                                     <div className="bottom-part">
-                                        <p className="bottom-title">Personnel médical</p>
-                                        <hr/>
-                                        <p className="bottom-number">01</p>
+                                        <p className="bottom-title">Personne lambda</p>
+                                        <hr className="hr-3"/>
+                                        <p className="bottom-number">03</p>
                                         <div className="clear"></div>
                                     </div>
                                 </div>
@@ -106,6 +106,28 @@ export default class BlockSec extends React.Component {
                             <div className="right">
                                 <div className="right-container">
                                     <img src="assets/img/illuu1.png" alt=""/>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="block__2-wrapper-r">
+                            <div className="left">
+                                <h2>App</h2>
+                                <div className="left-container">
+                                    <h3>Our application can be for everyone, that can be a doctor, a nurse, you, me, especially you.
+                                        Simplicity of use and cost will enable to endow this technology every person in the world.
+                                    </h3>
+                                    <div className="bottom-part">
+                                        <p className="bottom-title">App</p>
+                                        <hr className="hr-4"/>
+                                        <p className="bottom-number">04</p>
+                                        <div className="clear"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="right">
+                                <div className="right-container">
+                                    <img src="assets/img/illu4.png" alt=""/>
                                 </div>
                             </div>
                         </div>
